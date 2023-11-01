1d41875b56a246bb3d44f3be1af63ae4bed2a454
Lukas Jigberg <72192057+Luke-png@users.noreply.github.com>
2023-11-01 23:31:55 +0100

    Svg

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import rotateSVG from './resources/Courage.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-green-500">
-          Hello World!
-        </p>
+        <img src={rotateSVG} className="App-logo" alt="logo" />
+        {/*<p className="text-orange-500">Hello World!</p>*/}
       </header>
     </div>
   );
