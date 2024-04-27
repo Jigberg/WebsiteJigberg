f80b16829f1a77828505a2954791138ed29164ab
Lukas Jigberg <72192057+Jigberg@users.noreply.github.com>
2024-04-27 17:38:43 +0200

    Started  with screen response

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Link, NavLink } from "react-router-dom";
 
 const NavigationItem: React.FC<{ to: string; text: string }> = ({
@@ -13,31 +13,38 @@ const NavigationItem: React.FC<{ to: string; text: string }> = ({
 );
 
 const Header: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 720);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 720);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <header className="absolute z-10 w-screen flex justify-between">
-      {/* <div className="w-24 pl-4 pt-4">
-        <svg
-          viewBox="0 0 303 113"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="hover:animate-wiggle"
-        >
-          <path
-            d="M5 108L95.9053 28.7282C96.2819 28.3997 96.8431 28.3997 97.2197 28.7282L188.125 108M125.862 53.0866L179.612 5.58233C179.989 5.24933 180.554 5.2479 180.933 5.57901L298 108"
-            stroke="white"
-            stroke-width="12"
-            stroke-linecap="round"
-          />
-        </svg>
-      </div> */}
-      <nav className="pt-4 pr-4 text-white">
-        <ul className="flex">
-          <NavigationItem to="/" text="Home" />
-          <NavigationItem to="/viewModel" text="Models" />
-          <NavigationItem to="/rat" text="Rat" />
-          <NavigationItem to="/notFound" text="Contact" />
-        </ul>
-      </nav>
+    <header className="absolute z-10 w-screen justify-between">
+      {isMobile ? (
+        <nav className="text-white pt-4 px-5">
+          <ul className="flex flex-row justify-between">
+            <NavigationItem to="/" text="Home" />
+            <NavigationItem to="/viewModel" text="Models" />
+            <NavigationItem to="/rat" text="Rat" />
+            <NavigationItem to="/notFound" text="Contact" />
+          </ul>
+        </nav>
+      ) : (
+        <nav className="pt-4 pr-4 text-white">
+          <ul className="flex">
+            <NavigationItem to="/" text="Home" />
+            <NavigationItem to="/viewModel" text="Models" />
+            <NavigationItem to="/rat" text="Rat" />
+            <NavigationItem to="/notFound" text="Contact" />
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
