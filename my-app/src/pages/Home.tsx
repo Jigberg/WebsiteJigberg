6f64465ad74eb3f098fe6c7a6a72f0364a1b2356
Lukas Jigberg <72192057+Jigberg@users.noreply.github.com>
2024-05-03 02:17:01 +0200

    tired

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { memo, useRef, forwardRef } from "react";
 import { suspend } from "suspend-react";
-import { Canvas, useFrame, useThree } from "react-three-fiber";
+import { Canvas, useFrame, useThree, PerspectiveCameraProps } from "react-three-fiber";
 import {
   Stats,
   OrbitControls,
@@ -11,8 +11,10 @@ import {
   Sky,
   Environment,
   useGLTF,
+  Shadow,
   Loader,
   SpotLight,
+  PerspectiveCamera,
   CameraControls,
 } from "@react-three/drei";
 import Courage from "../resources/Courage.svg";
@@ -23,6 +25,12 @@ import "../css/Scroll.css";
 function MyModel() {
   const { scene } = useGLTF(record) as any;
   const modelRef = useRef();
+
+  useEffect(() => {
+    if (modelRef.current) {
+      (modelRef.current as THREE.Mesh).castShadow = true;
+    }
+  }, []);
 
   return (
     <primitive
@@ -66,15 +74,33 @@ function Home() {
 
   return (
     <div className="flex flex-col">
-      <div className="hiddenName h-screen flex items-center justify-center w-full ">
-        <Canvas
-          gl={{ preserveDrawingBuffer: true }}
-          dpr={1.5}
-          camera={{ position: [0, 40, 1], fov: 25 }}
-        >
-          <ambientLight intensity={1.5 * Math.PI} />
-          <Sky />
-          <group position={[0, 0, 0]} rotation={[0, 3, 0]}>
+      <div className="h-screen flex items-center justify-center w-full bg-[#BBBBBB]">
+        <Canvas className="hiddenName">
+          <PerspectiveCamera
+            makeDefault
+            position={[0, 2, 7]} // Set the position of the camera
+            rotation={[Math.PI / 1.1, Math.PI / 1, Math.PI / 1]}
+            fov={50}
+            aspect={window.innerWidth / window.innerHeight}
+            near={0.1}
+            far={1000}
+            onUpdate={(camera) => camera.updateProjectionMatrix()}
+          />
+          <directionalLight
+            castShadow
+            position={[0, 10, 0]}
+            intensity={1.5}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={50}
+            shadow-camera-left={-10}
+            shadow-camera-right={10}
+            shadow-camera-top={10}
+            shadow-camera-bottom={-10}
+          />
+          <spotLight position={[0, 5, 10]} angle={0.3} />
+
+          <group position={[0, 0, 0]} rotation={[0, 0, 0]}>
             <MyModel />
             <Backdrop
               receiveShadow
@@ -84,24 +110,24 @@ function Home() {
             >
               <meshPhysicalMaterial roughness={1} color="#efefef" />
             </Backdrop>
-            <rectAreaLight
-              args={["white", 3]}
-              width={5}
-              height={5}
-              position={[-3, 4, 1]}
-              visible={true}
-            />
           </group>
           <OrbitControls />
         </Canvas>
       </div>
-      <div className="hiddenName h-screen flex items-center justify-center w-full ">
-        <img
-          src={Courage}
-          className="w-4/6 animate-[spin_20s_linear_infinite] md:w-96"
-          alt="Courage"
-        />
+
+
+      <div className="h-screen w-full">
+        <div className="bg-gradient-to-b from-[#BBBBBB] to-[#282A3A] h-2/5" />
+        <div id="content" className="hiddenName h-full flex items-center justify-center">
+          <img
+            src={Courage}
+            className="w-4/6 animate-[spin_20s_linear_infinite] md:w-96"
+            alt="Courage"
+          />
+        </div>
+
       </div>
+
       <div className="hiddenName h-screen flex items-center justify-center w-full ">
         <img
           src={Courage}
